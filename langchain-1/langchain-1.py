cedfe1cf875c58c28054afb56dedf9da135509a3
syedabdullahbukhari77 <syedabdullahbukhari77@gmail.com>
2025-10-25 15:32:24 +0500

    langchain core updated (#03)

--- langchain-1/langchain-1.py
+++ langchain-1/langchain-1.py
@@ -2,4 +2,10 @@ from langchain_core.document import Document
 
 doc = Document(
     page_content='hey these are the words for langchain to describe the area | work;',
+    metadata={
+        'source' : 'example.txt',
+        'pages' : 1,
+        'author' : 'abdullah syed',
+        'date_created' : '2025-10-25'
+    }
 )
